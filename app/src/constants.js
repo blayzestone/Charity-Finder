68ba2d6603c916b8e2d2a914faa3c49a2dd66362
Blayze <blayzestone@protonmail.com>
2020-05-13 12:40:51 -0700

    initial project setup

---
+++ app/src/constants.js
@@ -0,0 +1,2 @@
+const baseUrl = "http://data.orghunter.com/v1/charitysearch";
+const apiKey = "6dcaccebced74f23cd97058e6ed1c5df";
---
+++ app/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import reducer from './store/reducers';
+
+import App from './App';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
